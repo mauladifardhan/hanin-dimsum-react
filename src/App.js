fb52217cf2a0d83a961583a4691fad410ef9673c
Fardhan Mauladi <fardhan16@gmail.com>
2022-10-02 15:45:19 +0700

    update new commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import {} from ".assets/css/app.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text">DIMSUM HANIN</h1>
+    </div>
+  );
+}
+
+export default App;
